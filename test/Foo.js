4cbad3e994c090198dcd3935ad41e01f8e24608f
Stephen Niedzielski <stephen@niedzielski.com>
2017-03-11 22:41:53 -0700

    chore: upgrade dependencies
    https://github.com/rndmem/eslint-config-rndmem/tree/v9.0.0#v900

--- test/Foo.js
+++ test/Foo.js
@@ -1,12 +1,14 @@
 import assert from 'assert'
+
 import skeleton from '../build/rndmem-npm-skeleton'
 
-describe('Foo', () => {
-  describe('bar()', () => {
+describe('Foo', function() {
+  describe('bar()', function() {
     const bar = skeleton.Foo.bar
 
-    it('return val', () => {
+    it('return val', function(done) {
       assert.deepEqual(bar(), 1)
+      done()
     })
   })
 })
